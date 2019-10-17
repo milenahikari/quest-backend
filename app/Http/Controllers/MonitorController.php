e0a620943e15785fe7233fb2cc4b451f2cf88eee
Milena <milenahikari11@gmail.com>
2019-10-17 20:23:03 -0300

    Salvando alteraçcoes

--- app/Http/Controllers/MonitorController.php
+++ app/Http/Controllers/MonitorController.php
@@ -64,7 +64,7 @@ class MonitorController extends Controller
     public function monitorDetail($idMonitor)
     {
         // if (!is_numeric($idMonitor) || $idMonitor < 0) return;
-
+        return $idMonitor;
         $monitor = Monitor::select(
             'monitors.id',
             'users.name as name',
